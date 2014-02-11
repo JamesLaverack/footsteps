9852662e18a5bfe7229e38be8fb655e9262cc1c4
James Laverack <james@jameslaverack.com>
2014-02-11 23:16:25 +0000

    Setup devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Footsteps::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # This is required for Devise to work
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
